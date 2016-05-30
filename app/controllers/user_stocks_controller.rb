10fd6f926f65df00f10e528dda7ca5ea928505bc
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-05-30 12:19:35 +0000

    show the list of followed stocks

--- app/controllers/user_stocks_controller.rb
+++ app/controllers/user_stocks_controller.rb
@@ -74,7 +74,7 @@ class UserStocksController < ApplicationController
   def destroy
     @user_stock.destroy
     respond_to do |format|
-      format.html { redirect_to user_stocks_url, notice: 'User stock was successfully destroyed.' }
+      format.html { redirect_to my_portfolio_path, notice: 'User stock was removed from portfolio.' }
       format.json { head :no_content }
     end
   end

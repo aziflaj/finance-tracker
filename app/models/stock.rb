83f952f6819a0a91098c44f03b03331f41045bee
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-05-30 12:09:34 +0000

    limit each user to 10 stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,7 @@
 class Stock < ActiveRecord::Base
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
   def self.find_by_ticker(ticker_symbol)
     where(ticker: ticker_symbol).first
   end

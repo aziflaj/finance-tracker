8aa9608e5da866319576f2201c4b371308aabcfb
Aldo Ziflaj <aldoziflaj95@gmail.com>
2016-06-22 17:52:52 +0000

    modify devise to register using full name

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {registrations: "user/registrations"} 
   resources :user_stocks, except: [:show, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
